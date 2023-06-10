b46a6c4ed05eb2859ac5b7fce9dc1840492d9700
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-10 07:40:55 +0300

    Creating categories list view item

---
+++ lib/user_interface/home/widgets/categories_list_view_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants/colors.dart';
+
+class CategoriesListViewItem extends StatelessWidget {
+  const CategoriesListViewItem(
+      {super.key, required this.text, required this.isSelected});
+  final String text;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 12),
+      child: MaterialButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        color: isSelected ? MyColors.purple : MyColors.black,
+        splashColor: isSelected ? MyColors.black : MyColors.purple,
+        height: 15,
+        onPressed: () {},
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+        ),
+      ),
+    );
+  }
+}
