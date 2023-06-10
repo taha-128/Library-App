b2e88c407e315bd3f4189edf46ff21ca67b0b888
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-10 07:41:12 +0300

    Adding Listview

--- lib/user_interface/home/widgets/home_screen_body.dart
+++ lib/user_interface/home/widgets/home_screen_body.dart
@@ -2,6 +2,7 @@
 //! Reomve this line ^
 
 import 'package:flutter/material.dart';
+import 'categories_list_view.dart';
 import 'custom_app_bar.dart';
 
 class HomeScreenBody extends StatelessWidget {
@@ -11,13 +12,15 @@ class HomeScreenBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 18),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
         width: double.infinity,
         height: double.infinity,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: const [
             CustomAppBar(),
+            SizedBox(height: 40),
+            CategoriesListView(),
           ],
         ),
       ),
