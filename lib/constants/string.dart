faf162cf415fca70566cd77d468d01fbb3235d4d
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-10 10:26:12 +0300

    Updating base url

--- lib/constants/string.dart
+++ lib/constants/string.dart
@@ -1,6 +1,10 @@
-const String baseUrl = 'https://www.googleapis.com/books/v1/volumes?q=api';
+const String baseUrl = 'https://firebasestorage.googleapis.com/v0/b/my-password-app-708ec.appspot.com/o/books_api.json?alt=media&token=66095a95-eee9-4418-b97e-20171a9d3214&_gl=1*1ftephe*_ga*MTU1OTQ4NjU0MS4xNjU2Nzc0NjM3*_ga_CW55HF8NVT*MTY4NjM4MTMxNC4zOS4xLjE2ODYzODE4NjYuMC4wLjA.';
 
 const String myFontFamily = 'SansArabic';
+
+const String loadingImage = 'assets/images/loading.gif';
+
+const String errorImage = 'assets/images/error.gif';
 
 const String kHomePath = '/';
 
