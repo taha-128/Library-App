017218f28517b3faafb1390585b19bcb6be9cbeb
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-10 16:39:49 +0300

    New icons

--- lib/utils/my_icons.dart
+++ lib/utils/my_icons.dart
@@ -4,4 +4,8 @@ class MyIcons {
   static const String kSearchIcon = 'assets/icons/search.svg';
 
   static const String kBookIcon = 'assets/icons/book.svg';
+
+  static const String kBookmarkIcon = 'assets/icons/bookmark.svg';
+
+  static const String kShareIcon = 'assets/icons/share.svg';
 }
