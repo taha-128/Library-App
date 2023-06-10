3cff1e7960778d292c6597faf6937366831327bf
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-10 06:25:52 +0300

    Adding new color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color pink = Color(0xFFba42a5);
+  static const Color lightPink = Color.fromARGB(255, 218, 70, 176);
   static const Color purple = Color(0xFF7f66dc);
   static const Color black = Color(0xFF121019);
   static const Color background = Color(0xFF121019);
