62b051b19ed0289800757d7ade55857f6be0cb66
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-10 06:58:03 +0300

    Changing the black color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,6 +4,6 @@ class MyColors {
   static const Color pink = Color(0xFFba42a5);
   static const Color lightPink = Color.fromARGB(255, 218, 70, 176);
   static const Color purple = Color(0xFF7f66dc);
-  static const Color black = Color(0xFF121019);
+  static const Color black = Color(0xFF211d2f);
   static const Color background = Color(0xFF121019);
 }
