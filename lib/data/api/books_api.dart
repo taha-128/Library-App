85a4681a2637739b4530a496a9fe4babc83c2907
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-12 21:31:32 +0300

    Creating Api class

--- lib/data/api/books_api.dart
+++ lib/data/api/books_api.dart
@@ -1,1 +1,24 @@
-const String baseUrl = 'https://firebasestorage.googleapis.com/v0/b/my-password-app-708ec.appspot.com/o/books_api.json?alt=media&token=66095a95-eee9-4418-b97e-20171a9d3214&_gl=1*1ftephe*_ga*MTU1OTQ4NjU0MS4xNjU2Nzc0NjM3*_ga_CW55HF8NVT*MTY4NjM4MTMxNC4zOS4xLjE2ODYzODE4NjYuMC4wLjA.';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+const String baseUrl =
+    'https://firebasestorage.googleapis.com/v0/b/my-password-app-708ec.appspot.com/o/arabic_books.json?alt=media&token=5f0c22bb-9fcb-41c5-b73e-452d6aa64b4c&_gl=1*10puqad*_ga*MTU1OTQ4NjU0MS4xNjU2Nzc0NjM3*_ga_CW55HF8NVT*MTY4NjUxMjYzMS40MC4xLjE2ODY1MTI2NjguMC4wLjA.';
+
+class BooksApi {
+  late Dio dio = Dio();
+
+  Future<List> getAllBooks() async {
+    try {
+      Response response = await dio.get(baseUrl);
+      if (kDebugMode) {
+        print(response.data['books']);
+      }
+      return response.data['books'];
+    } catch (e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
+      return [];
+    }
+  }
+}
