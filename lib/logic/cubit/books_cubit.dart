393e235812eaf32e01163883da14a838e6076b34
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-13 22:21:21 +0300

    Creating books cubit

--- lib/logic/cubit/books_cubit.dart
+++ lib/logic/cubit/books_cubit.dart
@@ -7,11 +7,39 @@ part 'books_state.dart';
 
 class BooksCubit extends Cubit<BooksState> {
   BooksCubit() : super(BooksInitial());
+
   late List<Book> books;
+  late List<Book> showedBooks = books;
+  List<String> categories = ['جميع الكتب'];
+  String category = 'جميع الكتب';
 
   Future<List<Book>> getAllBooks() async {
     books = await BooksRepository(booksApi: BooksApi()).getAllCharacters();
-    emit(BooksLoaded(books: books));
+    categories = await getCategories();
+    emit(BooksLoaded(books: showedBooks, categories: categories));
     return books;
+  }
+
+  Future<List<String>> getCategories() async {
+    for (int i = books.length - 1; i != -1; i--) {
+      String category = books[i].category!;
+      if (!categories.contains(category)) {
+        categories.add(category);
+      }
+    }
+    return categories;
+  }
+
+  Future<void> changeShowedBooks(String? category) async {
+    if (category == 'جميع الكتب') {
+      showedBooks = books;
+    } else {
+      showedBooks = books.where((book) => book.category == category).toList();
+    }
+    this.category = category ?? 'جميع الكتب';
+    emit(
+      CategoriesChanged(category: category ?? 'جميع الكتب', books: showedBooks),
+    );
+    // emit(BooksLoaded(books: books, categories: categories));
   }
 }
--- lib/logic/cubit/books_state.dart
+++ lib/logic/cubit/books_state.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'books_cubit.dart';
 
 abstract class BooksState {}
@@ -6,7 +5,14 @@ abstract class BooksState {}
 class BooksInitial extends BooksState {}
 
 class BooksLoaded extends BooksState {
-  BooksLoaded({required this.books});
+  BooksLoaded({required this.books, required this.categories});
   final List<Book> books;
+  final List<String> categories;
+}
 
+class CategoriesChanged extends BooksState {
+  CategoriesChanged({ required this.category, required this.books});
+  final String category;
+
+  final List<Book> books;
 }
