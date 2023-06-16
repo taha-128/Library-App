d62eb5bd5504d0a880ad71c45b4d0d4cb3e73c60
taha-128 <108621978+taha-128@users.noreply.github.com>
2023-06-17 02:59:20 +0300

    Adding searching field

--- lib/user_interface/home/widgets/custom_app_bar.dart
+++ lib/user_interface/home/widgets/custom_app_bar.dart
@@ -3,37 +3,59 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:library_app/utils/my_colors.dart';
 import 'package:library_app/utils/my_icons.dart';
 import 'package:library_app/utils/my_styles.dart';
+import 'my_search_field.dart';
 
-class CustomAppBar extends StatelessWidget {
+class CustomAppBar extends StatefulWidget {
   const CustomAppBar({super.key});
 
   @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+}
+
+bool isSearching = false;
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        GestureDetector(
-          child: SvgPicture.asset(
-            MyIcons.kSearchIcon,
-            width: 35,
-          ),
-        ),
-        RichText(
-          text: TextSpan(
+    TextEditingController controller = TextEditingController();
+    return isSearching
+        ? MySearchField(controller: controller)
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const TextSpan(
-                text: 'روايات',
-                style: MyStyles.textStyle26,
+              GestureDetector(
+                onTap: () {
+                  isSearching = true;
+                  ModalRoute.of(context)?.addLocalHistoryEntry(
+                    LocalHistoryEntry(
+                      onRemove: () {
+                        isSearching = false;
+                      },
+                    ),
+                  );
+                },
+                child: SvgPicture.asset(
+                  MyIcons.kSearchIcon,
+                  width: 35,
+                ),
               ),
-              TextSpan(
-                text: ' وكتب',
-                style: MyStyles.textStyle26.copyWith(color: MyColors.lightPink),
+              RichText(
+                text: TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: 'روايات',
+                      style: MyStyles.textStyle26,
+                    ),
+                    TextSpan(
+                      text: ' وكتب',
+                      style: MyStyles.textStyle26
+                          .copyWith(color: MyColors.lightPink),
+                    ),
+                  ],
+                ),
               ),
+              const Icon(Icons.menu, size: 40)
             ],
-          ),
-        ),
-        const Icon(Icons.menu, size: 40)
-      ],
-    );
+          );
   }
 }
---
+++ lib/user_interface/home/widgets/my_search_field.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:library_app/utils/my_styles.dart';
+import 'package:searchfield/searchfield.dart';
+
+import '../../../logic/cubit/books_cubit.dart';
+import '../../book_details/book_details_screen.dart';
+
+class MySearchField extends StatelessWidget {
+  const MySearchField({
+    super.key,
+    required this.controller,
+  });
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    BooksCubit cubit = BlocProvider.of<BooksCubit>(context);
+
+    return SearchField<String>(
+      searchStyle: MyStyles.textStyle18.copyWith(fontWeight: FontWeight.w500),
+      controller: controller,
+      suggestions: cubit.booksForSearch
+          .map(
+            (title) => SearchFieldListItem<String>(
+              title,
+              item: title,
+              child: Container(
+                width: double.infinity,
+                height: 200,
+                padding: const EdgeInsets.only(right: 12),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  title,
+                  textAlign: TextAlign.right,
+                  style: MyStyles.textStyle18.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          )
+          .toList(),
+      hint: 'ابحث عن أي كتاب',
+      onSubmit: (title) {
+        if (cubit.booksForSearch.contains(title)) {
+          controller.clear();
+          Navigator.pop(context);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => BookDetailsScreen(
+                book: cubit.books.firstWhere(
+                  (book) => book.title == title,
+                ),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
